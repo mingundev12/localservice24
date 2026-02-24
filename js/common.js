3c48ae2043d8cd4d05b3016cfe20759d5e184986
mingundev12 <mingundev12@gmail.com>
2026-02-24 13:43:37 +0900

    feat : favicon 로딩 함수, 페이지 불러오기 함수 수정

--- js/common.js
+++ js/common.js
@@ -1,17 +1,22 @@
 // 특정 url에 있는 내용을 selector(타겟) 에 집어넣는 함수
-async function includeHTML(url, selector) {
+async function includeFile(url, selector) {
     try {
         const response = await fetch(url);
         if(!response.ok) throw new Error('파일 불러오기 실패');
 
         const html = await response.text();
-        const target = document.querySelector(selector);
-
-        if(target) {
-            target.innerHTML = html;
-        }
+        includeHTML(html, selector);
     } catch (error) {
         console.error('HTML 로드 중 오류 발생 : ', error);
+    }
+}
+
+// 타겟에 html요소를 추가하는 함수
+function includeHTML(html, selector) {
+    const target = document.querySelector(selector);
+
+    if(target) {
+        target.innerHTML += html;
     }
 }
 
@@ -37,10 +42,20 @@ function removeClass(selector, className) {
     });
 }
 
-// header 와 footer 불러오기
-includeHTML('./header.html', '#header');
-includeHTML('./footer.html', '#footer');
-addClass('#header', 'color_header')
-addClass('#footer', 'color_footer')
+// header 와 footer 불러오기, 아이콘 교체
+async function initLayout() {
+    // header 불러오기
+    await includeFile('./header.html', '#header');
+    addClass('#header', 'color_header');
+
+    // footer 불러오기
+    await includeFile('./footer.html', '#footer');
+    addClass('#footer', 'color_footer');
+
+    // favicon 불러오기
+    const icon = `<link rel="icon" type="image/png" href="./img/logo_small.png">`;
+    includeHTML(icon, 'head');
+
+}
 
 // 메뉴 hover 효과
